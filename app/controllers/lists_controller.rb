5ed2fb523e69e4a8114c121ad7f027728c33dfeb
kevin thomas <kevindemorzine@gmail.com>
2022-11-18 15:50:38 +0100

    Rails Watch List

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -20,9 +20,14 @@ class ListsController < ApplicationController
     end
   end
 
+  def destroy
+    @list.destroy
+    redirect_to lists_path
+  end
+
   private
 
   def list_params
-    params.require(:list).permit(:name)
+    params.require(:list).permit(:name, :photo)
   end
 end
